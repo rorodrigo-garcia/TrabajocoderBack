4c467e13b0b3453d4cb28f0d3468155cc7723dc0
Rodrigar <rodriadriangarcia1997@gmail.com>
2023-03-10 04:02:42 -0300

    casi

--- productManager.js
+++ productManager.js
@@ -2,6 +2,7 @@ class ProductManager{
 
     constructor (){
         this.products= []
+        this.path = '/productos.json'
 
     }   
 
@@ -45,8 +46,52 @@ class ProductManager{
             }
          
          }
+
+        
 }
+
 const newProduct = new ProductManager ()
 newProduct.addProducts ({title:"tomate" , description :"Fruta versatil" , price : "300 el kilo" , thumbnails:"Sin imagen" , code:"t1" , stock:3 })
 newProduct.addProducts ({title:"cebolla" , description :"Verdura verde y versatil" , price : "250 el kilo" , thumbnails:"Sin imagen" , code:"C1" , stock:7 }) 
 newProduct.getProducts()
+
+const fs = require ('fs')
+
+const manejoArchivos = async()=>{
+
+    
+    try {
+        await fs.promises.writeFile('./productos.json' , JSON.stringify(newProduct, null,2) ,'utf-8')
+        .then(console.log("se creo el archivo")).catch((err)=>console.log(err))
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+    try {
+        await fs.promises.appendFile('./productos.json' , '  ' , 'utf-8')
+    } catch (error) {
+        console.log(error);
+    }
+
+    try {
+        if (fs.existsSync(this.path)) {
+            const products = await fs.promises.readFile(this.path,'utf-8')
+            let objetoParseado = JSON.parse(products)
+            return objetoParseado
+        } else {
+            return console.log("no se registro");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+    // try {
+    //     await fs.promises.unlink('./productos.json')
+    // } catch (error) {
+    //     console.log(error);
+    // } Lo comente para que se pueda leer el resto del codigo
+
+}
+
+manejoArchivos()
